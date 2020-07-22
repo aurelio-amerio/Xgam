00ec57550e1abc78c9666608e64b34aa95112092
Aurelio Amerio <aurelio.amerio@edu.unito.it>
2020-07-22 13:07:37 +0200

    up

--- Xgam/config/config_dataselection_6y_nside64.py
+++ Xgam/config/config_dataselection_6y_nside64.py
@@ -25,11 +25,11 @@ from Xgam.utils.ScienceTools_ import gtEbindef
 
 
 
-OUT_LABEL = 'w9w632_SV_t32'
+OUT_LABEL = 'w9w632_SV_t1_nside64'
 
 #start and stop week number to be selected in FT1 and FT2 files
 START_WEEK = 9
-STOP_WEEK = 632
+STOP_WEEK = 322
 
 # minumum and maximum energy and energy bin number to generate fits file
 E_MIN = 1000
@@ -42,7 +42,7 @@ SC_FILE = os.path.join(FT_DATA_FOLDER, 'spacecraft', 'lat_spacecraft_merged.fits
 # other settings
 ZMAX = 90
 EVCLASS = 2048
-EVTYPE = 32
+EVTYPE = 1
 IRFS = 'P8R3_SOURCEVETO_V2'
 HPX_MAP_ORDER = 6
 FILTER_CUT='DATA_QUAL==1&&LAT_CONFIG==1&&LAT_MODE==5&&IN_SAA!=T'+\
